9bc86a8f0e0ac0e0826fef307bf1bb6d529d6f57
ncguilbeault <ncguilbeault@gmail.com>
2026-08-20 18:14:05 +0100

    Rename to camelCase
    Co-authored-by: glopesdev <g.lopes@neurogears.org>

--- src/Bonsai.ML.Torch/LinearAlgebra/QRDecompositionResult.cs
+++ src/Bonsai.ML.Torch/LinearAlgebra/QRDecompositionResult.cs
@@ -5,15 +5,15 @@ namespace Bonsai.ML.Torch.LinearAlgebra;
 /// <summary>
 /// Represents the result of a QR decomposition.
 /// </summary>
-public readonly struct QRDecompositionResult((Tensor Q, Tensor R) result)
+public readonly struct QRDecompositionResult((Tensor q, Tensor r) result)
 {
     /// <summary>
     /// The orthogonal matrix Q.
     /// </summary>
-    public Tensor Q => result.Q;
+    public Tensor Q => result.q;
 
     /// <summary>
     /// The upper triangular matrix R.
     /// </summary>
-    public Tensor R => result.R;
+    public Tensor R => result.r;
 }
